85fe0737d84534c649fec6b46fd8ae838f5233e2
Juan Pedro Mancera Taboada <juanpedro.mancera.contractor@bbva.com>
2025-01-30 16:32:02 +0100

    Creado el componente principal

---
+++ src/app/components/cart-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cart-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-app.component.html',
+})
+export class CartAppComponent {}
