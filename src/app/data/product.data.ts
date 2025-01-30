c2d0b1d36776c8154b5629423e3ab91cc6a0b905
Juan Pedro Mancera Taboada <juanpemt@gmail.com>
2025-01-30 16:50:44 +0100

    Añadido datos de productos y servicio

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,39 @@
+import { Product } from '../models/product';
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Teclado mecanico Asus RGB',
+    description: 'Teclado mecanico con swicht cherry red',
+    price: 10.99,
+  },
+  {
+    id: 2,
+    name: 'Mouse mecanico Asus',
+    description: 'Mouse mecanico con swicht red',
+    price: 115.99,
+  },
+  {
+    id: 3,
+    name: 'Mouse wireless Logitech',
+    description: 'Mouse wireless con marco de color rojo',
+    price: 7.98,
+  },
+  {
+    id: 4,
+    name: 'Mouse wireless Genius',
+    description: 'Mouse wireless con marco de color azul',
+    price: 16.95,
+  },
+  {
+    id: 5,
+    name: 'Mouse wireless Genius',
+    description: 'Mouse wireless con marco de color verde',
+    price: 46.97,
+  },
+  {
+    id: 6,
+    name: 'Mouse wireless Genius',
+    description: 'Mouse wireless con marco de color amarillo',
+    price: 56.99,
+  },
+];
